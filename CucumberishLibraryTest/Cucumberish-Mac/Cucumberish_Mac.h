b8c7402314a5d92e6ed6bf1a88266dd678b267b7
Saul Mora <saul@casademora.com>
2018-11-08 11:39:04 -0800

    Fix up targets

---
+++ CucumberishLibraryTest/Cucumberish-Mac/Cucumberish_Mac.h
@@ -0,0 +1,19 @@
+//
+//  Cucumberish_Mac.h
+//  Cucumberish-Mac
+//
+//  Created by Saul Mora on 9/10/18.
+//  Copyright © 2018 Ahmed Ali. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Cucumberish_Mac.
+FOUNDATION_EXPORT double Cucumberish_MacVersionNumber;
+
+//! Project version string for Cucumberish_Mac.
+FOUNDATION_EXPORT const unsigned char Cucumberish_MacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cucumberish_Mac/PublicHeader.h>
+
+
